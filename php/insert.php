55aa2fa1151d908682cf6ac0f9b1daa4d5912cc5
Cloud User <ec2-user@ip-10-0-0-238.ap-northeast-1.compute.internal>
2020-12-17 16:33:46 +0000

    password is changed from my-secret-pw to root

--- php/insert.php
+++ php/insert.php
@@ -1,9 +1,8 @@
 <?php
   $user = 'root';
-  $password = getenv('MYSQL_ROOT_PASSWORD');
+  $password = 'root';
   $dbName = 'chapter6_db';
-  $dbServer = getenv('DB_SERVER');
-  $host = "{$dbServer}:8889";
+  $host = 'mysql:8889';
   $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
 ?>
 
